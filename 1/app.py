fc8d39d2ca8edd44bc2489a22f6d3fcdffd291c1
lvyaojia <lvyaojia@douban.com>
2013-04-26 02:27:40 +0800

    finish architecture

--- 1/app.py
+++ 1/app.py
@@ -6,17 +6,10 @@ from bae.core.const import APP_NAME, APP_DIR, APP_TMPDIR
 from bae.core.wsgi import WSGIApplication
 sys.path.insert(0, APP_DIR + '/lib')
 
-from weixin.api import weixin_api
+from weixin import weixin_api
 
 app = Flask(__name__)
 app.debug = True
-app.register_blueprint(weixin_api)
-
-
-@app.route('/aa')
-def hello():
-    return "Hello, world! - Flask\n"
-
-
+app.register_blueprint(weixin_api, url_prefix='/weixin')
 
 application = WSGIApplication(app.wsgi_app)
--- 1/local.py
+++ 1/local.py
@@ -3,15 +3,11 @@ import sys
 
 from flask import Flask
 
-from weixin.api import weixin_api
+from weixinimport weixin_api
 
 app = Flask(__name__)
 app.debug = True
-app.register_blueprint(weixin_api)
-
-@app.route('/')
-def hello():
-    return "Hello, world! - Flask\n"
+app.register_blueprint(weixin_api, url_prefix='/weixin')
 
 if __name__ == '__main__':
     app.run()
