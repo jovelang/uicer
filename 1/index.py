b0ec32914bcb4b9125b0d24c83938f8c9cbdce71
lvyaojia <lvyaojia@douban.com>
2013-04-26 01:14:06 +0800

    init web

--- 1/index.py
+++ 1/index.py
@@ -1,16 +1,14 @@
 #-*- coding:utf-8 -*-
 import sys
-sys.path.insert(0, APP_DIR + '/lib')
 
 from flask import Flask
-
 from bae.core.const import APP_NAME, APP_DIR, APP_TMPDIR
 from bae.core.wsgi import WSGIApplication
+sys.path.insert(0, APP_DIR + '/lib')
 
 app = Flask(__name__)
 app.debug = True
 application = WSGIApplication(app)
-
 
 @app.route('/')
 def index():
